d9712f602e2bc49437b7ba0e031f3d4af6b02613
Pingaksh2K24 <pingakshinnovations24@gmail.com>
2025-09-26 19:09:56 +0530

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import LaunchGrid from '@/components/Launch/LaunchGrid';
+import LaunchDetail from '@/components/Launch/LaunchDetail';
+import FavoritesPanel from '@/components/Favourites/FavoritesPanel';
+import ErrorMessage from '@/components/ErrorMessage/page';
+import { useState } from 'react';
+import { Launch } from '@/types/spacex';
+
+export default function Home() {
+  const [selectedLaunch, setSelectedLaunch] = useState<Launch | null>(null);
+  const [showError, setShowError] = useState(false);
+  const [showFavorites, setShowFavorites] = useState(false);
+
+  return (
+    <div className="min-h-screen bg-gray-50 p-6">
+      <div className="max-w-7xl mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
+          <div className="lg:col-span-8">
+            {showFavorites ? (
+              <FavoritesPanel 
+                onBack={() => setShowFavorites(false)}
+                onLaunchSelect={setSelectedLaunch}
+              />
+            ) : (
+              <LaunchGrid 
+                onLaunchSelect={setSelectedLaunch}
+                onShowFavorites={() => setShowFavorites(true)}
+              />
+            )}
+          </div>
+          <div className="lg:col-span-4 space-y-6">
+            <LaunchDetail selectedLaunch={selectedLaunch} />
+            {showError && <ErrorMessage />}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
